71af458bdb80620faf967f057b589ceadde341e3
Wasim Hanif <wasim.hanif@simplifyvms.com>
2023-09-04 14:49:23 +0500

    'welcome'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit  <code>src/App.js</code> and save and Running.
+          Welcome Into Riemann System (PVT.) LTD
         </p>
         <a
           className="App-link"

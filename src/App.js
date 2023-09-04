fc62e4583a98bdf7ce43ff164ceca932814a42b6
Wasim Hanif <wasim.hanif@simplifyvms.com>
2023-09-04 15:55:55 +0500

    'undo'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState} from 'react'
 
 function App() {
-	// The state for our timer
-	const [timer, setTimer] = useState(new Date().getTime());
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +9,6 @@ function App() {
         <p>
           Wellcome Into Riemann System (PVT.) LTD
         </p>
-        <h2>{timer}</h2>
 
       </header>
     </div>

6b7b3362f2de2e5c4d7df574c27e542a87717fd0
Wasim Hanif <wasim.hanif@simplifyvms.com>
2023-09-04 14:42:23 +0500

    'update'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit  <code>src/App.js</code> and save to reload hello world dfsf.
+          Edit  <code>src/App.js</code> and save and Running.
         </p>
         <a
           className="App-link"
